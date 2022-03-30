840f72d9485bc5ced01efba2c1b61df1651ad8d8
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-03-30 11:14:36 +0700

    Create Lab5Bai1.java

---
+++ Lab1/src/Bai1/Lab5Bai1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bai1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nguyễn Văn Hải Long
+ */
+public class Lab5Bai1 {
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào giá trị góc (độ):");
+        int goc = scanner.nextInt();
+        double radian = Math.toRadians(goc);
+        System.out.println("Giá trị góc theo Radian là: "+radian);
+    }
+}
