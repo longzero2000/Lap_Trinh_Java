0f3225328fc33df85ec4604a26e904eb9a654212
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-03-30 11:29:19 +0700

    Update Lab2Bai1.java

--- source/src/Lab2/Lab2Bai1.java
+++ source/src/Lab2/Lab2Bai1.java
@@ -4,10 +4,32 @@
  */
 package Lab2;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 /**
  *
  * @author Nguyễn Văn Hải Long
  */
 public class Lab2Bai1 {
-    
+    public static void main(String[] args) {
+        double nghiem;
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số a: ");
+        int a = scanner.nextInt();
+        System.out.println("Nhập vào số b: ");
+        int b = scanner.nextInt();
+        System.out.println("Phương trình bạn vừa nhập vào là: " + a + "x + " + b + " = 0.");
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phương trình này có vô số nghiệm.");
+            } else {
+                System.out.println("Phương trình vô nghiệm.");
+            }
+        } else {
+            nghiem = (double) -b / a;
+            System.out.println("Phương trình có nghiệm x = " + decimalFormat.format(nghiem) + ".");// Làm tròn đến chữ số thập phân thứ 2
+        }
+    }
 }
