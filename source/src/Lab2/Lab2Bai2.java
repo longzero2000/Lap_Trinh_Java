e570319a4688fcced7c44083300fea2d6cc7fac2
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-03-30 12:16:42 +0700

    Create Lab2Bai2.java

---
+++ source/src/Lab2/Lab2Bai2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nguyễn Văn Hải Long
+ */
+public class Lab2Bai2 {
+    public static void main(String[] args) {
+        double a, b, c, x1, x2, delta;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Nhập a (a # 0): ");
+            a = scanner.nextDouble();
+        } while (a == 0);
+        System.out.print("Nhập b: ");
+        b = scanner.nextDouble();
+        System.out.print("Nhập c: ");
+        c = scanner.nextDouble();
+        delta = Math.pow(b, 2) - 4 * a * c;
+        if (delta < 0) {
+            System.out.println("Phương trình vô nghiệm!");
+        } else if (delta == 0) {
+            x1 = -b/(2*a);
+            System.out.println("Phương trinh có nghiệm kép là x1 = x2 = "+x1);
+        } else {
+            x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("Phương trình có 2 nghiệm x1 = " + x1 + " và x2 = " + x2);
+        }
+    }
+}
