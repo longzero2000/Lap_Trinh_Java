906ea204820921c36470f0127ac90ac652efa3d6
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-03-31 22:39:48 +0700

    Create Lab3Bai2.java

---
+++ source/src/Lab3/Lab3Bai2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nguyễn Văn Hải Long
+ */
+public class Lab3Bai2 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; ++i){
+            for(int j = 1; j < 10; ++j)
+            {
+                System.out.printf("%d * %d = %d \t", j, i, i * j);
+            }
+        System.out.printf("\n");
+        }
+    }
+    
+}
