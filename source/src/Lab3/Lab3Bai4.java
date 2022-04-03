1193cd490ce31ea8158f3fa243c6fd10a1dafb7e
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2022-04-03 18:29:09 +0700

    Update

---
+++ source/src/Lab3/Lab3Bai4.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Lab3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nguyễn Văn Hải Long
+ */
+public class Lab3Bai4 {
+    static String HoTen[] = new String[50];
+    static double Diem[] = new double[50];
+    static int index = 0;
+    static int n;
+    //Nhập
+    static void Nhap(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập số lượng sinh viên:");
+        n = sc.nextInt();
+        for(index=0;index<n;index++){
+            sc = new Scanner(System.in);
+            System.out.printf("Nhập sinh viên thứ %d:\n",index+1);
+            System.out.println("Nhập họ và tên:");
+            HoTen[index] = sc.nextLine();
+            System.out.print("Nhập điểm:");
+            Diem[index] = sc.nextDouble();
+        }
+    }
+    //Xuất
+    static void Xuat(){
+        System.out.println("Danh sách sinh viên");
+        for(int i=0; i<index;i++){
+            System.out.println("\n"+i+".Tên:" + HoTen[i]+"\nĐiểm "+Diem[i]);
+        } 
+    }
+    
+    public static void main(String[] args) {
+        Nhap();
+        Xuat();
+    }
+}
